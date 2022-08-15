10a885a940e0b647cafb1800d06f3a384c275ef5
fatih1126 <77747382+fatih1126@users.noreply.github.com>
2022-08-15 11:17:52 +0300

    Update webpack.common.js

--- threejs-graces-main/bundler/webpack.common.js
+++ threejs-graces-main/bundler/webpack.common.js
@@ -27,66 +27,48 @@ module.exports = {
     ],
     module:
     {
-        rules:
-        [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use:
-                [
-                    'html-loader'
-                ]
-            },
-
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
-            },
-
-            // CSS
-            {
-                test: /\.css$/,
-                use:
-                [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
                 ]
             },
 
             // Images
             {
                 test: /\.(jpg|png|gif|svg)$/,
-                type: 'asset/resource',
-                generator:
-                {
-                    filename: 'assets/images/[hash][ext]'
-                }
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/images/'
+                        }
+                    }
+                ]
             },
 
             // Fonts
             {
                 test: /\.(ttf|eot|woff|woff2)$/,
-                type: 'asset/resource',
-                generator:
-                {
-                    filename: 'assets/fonts/[hash][ext]'
-                }
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/fonts/'
+                        }
+                    }
+                ]
             },
 
             // Shaders
             {
                 test: /\.(glsl|vs|fs|vert|frag)$/,
-                type: 'asset/source',
-                generator:
-                {
-                    filename: 'assets/images/[hash][ext]'
-                }
-                }
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader',
+                    'glslify-loader'
+                ]
+            }
         ]
     }
 }
